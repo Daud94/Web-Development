b1c25a72f982d3cfbd102e6b02cf577717bf2683
Daud Olarewaju <olarewajuyahyadaud@gmail.com>
2023-01-05 18:11:37 +0100

    drum kit

--- Drum Kit/index.js
+++ Drum Kit/index.js
@@ -1,7 +1,34 @@
 document.querySelectorAll(".drum").forEach(drum => {
     drum.addEventListener("click",function (){
         let buttonInnerHTML = this.innerHTML; // or drum.innerHTML
+        animateButton(buttonInnerHTML);
         switch (buttonInnerHTML){
+            case "w":
+                playAudio("crash.mp3");
+                break;
+            case "a":
+                playAudio("kick-bass.mp3");
+                break;
+            case "s":
+                playAudio("snare.mp3");
+                break;
+            case "d":
+                playAudio("tom-1.mp3");
+                break
+            case "j":
+                playAudio("tom-2.mp3");
+                break
+            case "k":
+                playAudio("tom-3.mp3");
+                break
+            default:
+                playAudio("tom-4.mp3");
+        }
+    })
+    drum.addEventListener("keypress",function (event) {
+        let key = event.key
+        animateButton(key);
+        switch (key){
             case "w":
                 playAudio("crash.mp3");
                 break;
@@ -29,4 +56,10 @@ document.querySelectorAll(".drum").forEach(drum => {
 function playAudio(song){
     let audio = new Audio(`sounds/${song}`);
     audio.play();
+}
+function animateButton(currentKey){
+    document.querySelector(`.${currentKey}`).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector(`.${currentKey}`).classList.remove("pressed");
+    },300);
 }
